41f099057aefdfb91dd1278c97f6bfeae99ed772
yunshin <815choyunsin@gmail.com>
2022-12-10 15:44:31 +0900

    Update NoteManager.cs
    추가 안쓰는 parameter 처냄

--- 03_submission_version/unity/Assets/0test/01_game_scripts/NoteManager.cs
+++ 03_submission_version/unity/Assets/0test/01_game_scripts/NoteManager.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 public class NoteManager : MonoBehaviour
 {
     public GameObject goUI = null;
@@ -13,18 +11,11 @@ public class NoteManager : MonoBehaviour
     Pause ps;
     NewStartMenu NSM;
     public bool IsPause;
-    public int bpm = 0;
-    double currentTime = 0d;
     int blockcnt = 0;
     int blockcnt1 = 0;
     double cnt = 0d;
     double cnt1 = 0d;
     double totalTime = 0d;
-    public static bool p = true;
-    public static bool q = true;
-    public static bool p1 = true;
-    public static bool q1 = true;
-    public static bool z = true;
     bool musicStart = false;
     public Transform NoteAppear = null;
     public GameObject goNote = null;
@@ -61,7 +52,6 @@ public class NoteManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentTime += Time.deltaTime * 2;        
         rnd = UnityEngine.Random.Range(1, 3);
         rnd_y = UnityEngine.Random.Range(1, 3);
         v3 = new Vector3(NoteAppear.position.x, NoteAppear.position.y +(1.2f)*rnd_y -0.7f ,  rnd * 2.6f - 4.1f);
@@ -122,5 +112,5 @@ public class NoteManager : MonoBehaviour
         IsPause = true;        
         return;
     }
-
+    
 }
